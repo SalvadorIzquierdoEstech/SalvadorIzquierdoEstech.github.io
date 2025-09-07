ce466a8717ab505b31c38c6bdf5e136afb662984
Salvador.izquierdo <salvador.izquierdo@escuelaestech.es>
2025-09-07 21:31:29 +0200

    fixing the page

--- animation.js
+++ animation.js
@@ -137,3 +137,21 @@ prev.onclick = function () {
 
 
 
+// Divisor de secciones
+
+let sections = document.querySelectorAll('section');
+
+window.onscroll = () => {
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop;
+        let height = section.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+            section.classList.add('active');
+        } else {
+            section.classList.remove('active');
+        }
+
+    })
+}
